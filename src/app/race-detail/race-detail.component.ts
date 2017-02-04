2ddd7ef82e577062b4b126856c74505e62fdcae7
Sam <orgillsams@gmail.com>
2017-02-04 13:58:02 +0000

    refactoring

--- src/app/race-detail/race-detail.component.ts
+++ src/app/race-detail/race-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'app-race-detail',
